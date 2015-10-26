9508d2ceee41807a9c1b4f39b85d819c6fb24714
Ross Crawford-d'Heureuse <rosscdh@MacBook-Pro-4.local>
2015-10-26 14:42:00 +0100

    working

--- new-raspberry/roles/raspberry_livestream/files/stream.sh
+++ new-raspberry/roles/raspberry_livestream/files/stream.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 # Run avconv to stream the webcam's video to the RTMP server.
-
+if [ -c /dev/video0 ]; then
 /usr/bin/avconv  -f video4linux2 -s 320x240 -r "10" -i /dev/video0 -pix_fmt yuv420p -preset ultrafast -f flv -an rtmp://127.0.0.1/live/video0
+fi
