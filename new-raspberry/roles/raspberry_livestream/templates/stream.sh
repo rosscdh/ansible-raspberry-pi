131e1a028c0cd4b0a63d2ed0cc9f9fd1eccd3d2e
Ross Crawford-d'Heureuse <rosscdh@MacBook-Pro-4.local>
2015-10-30 16:39:40 +0100

    fixed video

--- new-raspberry/roles/raspberry_livestream/templates/stream.sh
+++ new-raspberry/roles/raspberry_livestream/templates/stream.sh
@@ -2,5 +2,5 @@
 
 # Run avconv to stream the webcam's video to the RTMP server.
 if [ -c {{ video_device_path }} ]; then
-/usr/bin/avconv -f video4linux2 -s 320x240 -r 10 -i /dev/video0 -vf drawtext="fontfile=/usr/share/fonts/truetype/freefont/FreeSansBold.ttf:text='{{ title }}':fontsize=16:fontcolor=blue:x=2:y=360" -acodec copy -metadata title='{{ title }}' -f flv -an {{ rtmp_server }}
+/usr/bin/avconv -f video4linux2 -s 160x120 -r 15 -pix_fmt monow -i /dev/video0 -vf drawtext="fontfile=/usr/share/fonts/truetype/freefont/FreeSansBold.ttf:text='{{ title }}':fontsize=16:fontcolor=blue:x=2:y=360" -an -metadata title='{{ title }}' -f flv -an {{ rtmp_server }}
 fi
